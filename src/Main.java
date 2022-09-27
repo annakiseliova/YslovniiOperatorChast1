6b22940a02283d438354b6f0b32a71ea030e6884
Анна <annakiseliova7@gmail.com>
2022-09-27 16:29:03 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 2
-        System.out.println("Задание 2");
-        int age = 18;
-        if(age >= 7 && age < 18) {
-        System.out.println("Иди в школу");
+        //Задание 3
+        System.out.println("Задание 3");
+        int wagonCapacity = 102;
+        int sitPlace = 60;
+        int place = 105;
+        if(place <= sitPlace){
+            System.out.println("В вагоне есть сидячее место");
+        }
+        if(place > sitPlace && place <= wagonCapacity ){
+            System.out.println("В вагоне нет сидячих мест, есть только стоячее место");
+        }
+        if(place > wagonCapacity){
+            System.out.println("Мест нет, вагон полностью забит");
+        }
     }
-        if(age >= 18 && age < 24 ) {
-            System.out.println("Иди в университет");
-        }
-        if(age >= 24){
-            System.out.println("Ищи свою первую работу");
-        }
-        }
 }
+
+
+
+
+
+
+
