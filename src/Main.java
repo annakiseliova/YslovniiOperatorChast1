82cd3dc4347d34273bd300e96a01ccb3b1cbe079
Анна <annakiseliova7@gmail.com>
2022-09-27 15:47:08 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 25;
+        if(age >= 18) {
+        System.out.println("Поздравляю, ты можешь водить!");
+    }
+        if(age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, ты не мошешь водить, нужно немного подождать");
+        }
+        }
+}
